4607237246a6d563a61b9bf0dfab34338095c4c7
Carsten Witt <carsten.witt@gmail.com>
2016-12-16 14:15:31 +0100

    New Database test base class

--- tests/src/DatabaseTestCaseAbstract.php
+++ tests/src/DatabaseTestCaseAbstract.php
@@ -30,8 +30,6 @@ abstract class DatabaseTestCaseAbstract extends \PHPUnit_Extensions_Database_Tes
     }
 
 
-
-
     /**
      * @return PDO
      */
@@ -41,43 +39,5 @@ abstract class DatabaseTestCaseAbstract extends \PHPUnit_Extensions_Database_Tes
     }
 
 
-
-
-
-    /**
-     * Creates a temporary copy of the given table
-     * May be useful one day.
-     *
-     * @param $tableName
-     */
-    public function createTmpTable($tableName)
-    {
-        $this->getPdo()->query( sprintf('DROP TEMPORARY TABLE IF EXISTS tmp_%1$s;', $tableName));
-        $this->getPdo()->query( sprintf('CREATE TEMPORARY TABLE tmp_%1$s LIKE %1$s;', $tableName));
-        $this->getPdo()->query( sprintf('INSERT tmp_%1$s SELECT * FROM %1$s;', $tableName));
-    }
-
-    /**
-     * Restores a given table from a previously copied temporary table
-     * May be useful one day.
-     *
-     * @param $tableName
-     */
-    public function restoreFromTmpTable($tableName)
-    {
-        $this->getPdo()->query( sprintf('DROP TABLE %1$s;', $tableName));
-        $this->getPdo()->query( sprintf('CREATE TABLE %1$s LIKE tmp_%1$s;', $tableName));
-        $this->getPdo()->query( sprintf('INSERT %1$s SELECT * FROM tmp_%1$s;', $tableName));
-        $this->getPdo()->query( sprintf('DROP TEMPORARY TABLE IF EXISTS tmp_%1$s;', $tableName));
-    }
-
-
-    /**
-     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
-     */
-    protected function getDataSet()
-    {
-        return $this->createMySQLXMLDataSet( __DIR__ . "/auth_logins-dataset.xml");
-    }
 
 }
